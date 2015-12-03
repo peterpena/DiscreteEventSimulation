50cd5af40d20a920dd6f78481afa5f38197beb38
peterpena <peter.a.pena@gmail.com>
2015-12-03 16:10:35 -0500

    Added Exponential Mean And Algorithm

---
+++ ExponentialMeanTest/server.h
@@ -0,0 +1,29 @@
+#ifndef SERVER_H
+#define SERVER_H
+#define NONVALID -1;
+#include "job.h"
+#include <vector>
+
+using namespace std;
+
+class Server
+{
+    public:
+        Server();
+        virtual ~Server();
+        int ServerID = NONVALID;
+        int TotalJobsServed = 0;
+        int NumCurrentJobs = 0;
+        int Capacity = 0;
+        int TotalJobsArrived = 0;
+        int TotalOldJobsArrived = 0;
+        int TotalJobsDenied = 0;
+        int TotalArrivingJobsDenied = 0;
+        int TotalArrivingJobs = 0;
+        vector<int> JobCount;
+        vector<Job> Jobs;
+
+        void initializeJobCount(int JobCountNum);
+};
+
+#endif // SERVER_H
